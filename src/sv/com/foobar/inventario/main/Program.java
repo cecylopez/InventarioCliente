2edc34a6888e1baf375d7fca8589bd101a384163
robertux <blendboy@gmail.com>
2016-01-26 01:32:22 -0600

    Adición de líneas en el main para pruebas DAO

--- src/sv/com/foobar/inventario/main/Program.java
+++ src/sv/com/foobar/inventario/main/Program.java
@@ -1,20 +1,31 @@
 package sv.com.foobar.inventario.main;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 import sv.com.foobar.inventario.controllers.ControladorSeguridad;
+import sv.com.foobar.inventario.dao.AdminConexiones;
 import sv.com.foobar.inventario.entities.Rol;
 
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ControladorSeguridad cs= new ControladorSeguridad();
-		List<Rol> roles=cs.getRoles();
-		System.out.println("Hay "+ roles.size()+ " en la base");
-		System.out.println("Mostrandoe el rol"+ roles.get(0));
+	public static void main(String[] args) throws SQLException {
+//		ControladorSeguridad cs = new ControladorSeguridad();
+//		List<Rol> roles = cs.getRoles();
+//		System.out.println("Hay " + roles.size() + " roles en la base de datos");
+//		System.out.println("Rol: " + roles.get(0));
+		Map<String,String> datos=new AdminConexiones().getDatosConexion();
+		System.out.println("url:" + datos.get("url"));
+		System.out.println("user:" + datos.get("user"));
+		System.out.println("password: "+ datos.get("password"));
 		
-
+		Connection cxn= new AdminConexiones().getConexion();
+		
+		System.out.println("Estado conexion " + cxn.isClosed());
 	}
 
 }
